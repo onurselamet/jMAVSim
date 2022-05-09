746a5f34c09686bf5d4bf1f50a8baaae27f6ab80
Romain Chiappinelli <romain.chiap@gmail.com>
2022-05-09 13:42:53 +0200

    display in xpc

--- src/me/drton/jmavsim/MAVLinkDisplayOnly.java
+++ src/me/drton/jmavsim/MAVLinkDisplayOnly.java
@@ -5,6 +5,10 @@ import me.drton.jmavlib.mavlink.MAVLinkMessage;
 import me.drton.jmavlib.mavlink.MAVLinkSchema;
 import me.drton.jmavsim.vehicle.AbstractVehicle;
 
+import gov.nasa.xpc.XPlaneConnect;
+import java.io.IOException;
+import java.net.SocketException;
+
 import javax.vecmath.*;
 import java.util.Arrays;
 
@@ -22,10 +26,15 @@ public class MAVLinkDisplayOnly extends MAVLinkHILSystemBase {
     private double alt0;                // initial altitude (meters)
     private double lat;                 // geodetic latitude (radians)
     private double lon;                 // geodetic longitude (radians)
-    private double alt;                  // above sea level (meters)
+    private double lat_deg;             // geodetic latitude (degrees)
+    private double lon_deg;             // geodetic longitude (degrees)
+    private double alt;                 // above sea level (meters)
+    private float roll_deg, pitch_deg, yaw_deg; // euler angles in degrees (to represent vehicle attitude)
+    private float gears_out=1.0f;       // set to 0 to display the landing gears inside in X plane
     private double [] quat={0.0,0.0,0.0,0.0};   // unit quaternion for attitude representation
     private Double [] control = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
     private static final double EARTH_RADIUS=6371000.0;    // earth radius in meters
+    private XPlaneConnect xpc=null;
 
     /**
      * Create MAVLinkDisplayOnly, MAVLink system that sends nothing to autopilot and passes states from
@@ -53,17 +62,42 @@ public class MAVLinkDisplayOnly extends MAVLinkHILSystemBase {
             for (int i = 0; i < 4; ++i) {
                 quat[i] = ((Number)((Object[])msg.get("attitude_quaternion"))[i]).doubleValue();
             }
-            lat=Math.toRadians(msg.getDouble("lat")*1e-7);
-            lon=Math.toRadians(msg.getDouble("lon")*1e-7);
+            lat_deg=msg.getDouble("lat")*1e-7;
+            lon_deg=msg.getDouble("lon")*1e-7;
+            lat=Math.toRadians(lat_deg);
+            lon=Math.toRadians(lon_deg);
             alt=msg.getDouble("alt")*1e-3;
 
             Vector3d pos = new Vector3d(EARTH_RADIUS*(lat-lat0),2.0+EARTH_RADIUS*(lon-lon0)*Math.cos(lat0),alt0-alt);
             double [] euler = RotationConversion.eulerAnglesByQuaternion(quat);
             Matrix3d dcm = new Matrix3d(RotationConversion.rotationMatrixByEulerAngles(euler[0],euler[1],euler[2]));
+            roll_deg=(float)Math.toDegrees(euler[0]);
+            pitch_deg=(float)Math.toDegrees(euler[1]);
+            yaw_deg=(float)Math.toDegrees(euler[2]);
 
             vehicle.setControl(Arrays.asList(control));     // set 0 throttles
             vehicle.setPosition(pos);   // we want ideally a "local" pos groundtruth
             vehicle.setRotation(dcm);
+
+            // send the vehicle position and orientation to Xplane via Xplane connect
+            try {
+                if (xpc==null) {
+                    xpc = new XPlaneConnect();
+                    // Ensure connection established.
+                    xpc.getDREF("sim/test/test_float");
+                    // disable the x-plane physics engine so the vehicle won't fall
+                    // xpc.sendDREF("sim/operation/override/override_planepath",1.0f);
+                    // xpc.pauseSim(true);   // pausing X plane simulator engine
+                    // xpc.sendCTRL(ctrl);
+                }
+                double[] posi = new double[] {lat_deg, lon_deg, alt, pitch_deg, roll_deg, yaw_deg};
+                // float[] posi = new float[] {37.524F, -122.06899F, 2500, 0, 0, 0, 1};
+                xpc.sendPOSI(posi);
+            } catch (SocketException ex) {
+                System.out.println("Unable to set up the connection. (Error message was '" + ex.getMessage() + "'.)");
+            } catch (IOException ex) {
+                System.out.println("Something went wrong with one of the commands. (Error message was '" + ex.getMessage() + "'.)");
+            }
         }
     }
 
